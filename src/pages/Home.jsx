b06398a344a5d739bba6ae0a938f7fce7e8352cb
Daniel Vergara <danielvsuarez01@gmail.com>
2022-01-21 21:50:23 -0500

    header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
-
+import Header from '../components/Header'
 const Home = () => {
-  return (
-    <div>
-      <h1>React Shop</h1>
-    </div>
-  )
+  return <Header></Header>
 }
 
 export default Home
